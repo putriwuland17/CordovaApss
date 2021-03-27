e51838fa97379ef105b91a9222611d327f490381
putriwuland17 <putri1710.wulandari@gmail.com>
2021-03-27 18:45:11 +0700

    projectapp

---
+++ www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id16458878_putriwulan","%ADI[u9<SKV<DbPW","id16458878_kisekimirai") or die ("could not connect database");
+?>
